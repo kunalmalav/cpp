b3fc9b951f983cc880681482f4408a853072d58d
Kunal Malav <kunalmalavno.1@gmail.com>
2022-08-11 22:37:24 +0530

    add all calculation

--- main.cpp
+++ main.cpp
@@ -4,9 +4,24 @@ using namespace std;
 
 int main()
 {
-    string s;
-    cout<<"may i know your fathers name: ";
-    getline(cin,s);
-    cout<<s;
-    return 0;
+    int a,b,c;
+    cout<<"enter two numbers";
+    cin>>a>>b;
+
+//for sum
+    c=a+b;
+    cout<<c<<endl;
+
+//for sub
+    c=a-b;
+    cout<<c<<endl;
+
+//for multiply
+    c=a*b;
+    cout<<c<<endl;
+
+//for dividation
+    //c=a/b;
+    //cout<<c;
+return 0;
 }
