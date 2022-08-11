a6eb40b1a26b3ee2d5fa850583fdbf04dd5767f7
Kunal Malav <kunalmalavno.1@gmail.com>
2022-08-12 03:27:34 +0530

    setting bitwise operaters

--- main.cpp
+++ main.cpp
@@ -1,12 +1,37 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-        char x=127,y=-128;
-        x++;
-        y--;
+int main()
+{
+    int x=11,y=5,z;
+    int a=11,b=7,c;
+    int d=11,e=7,f;
+    char j=5,h;
+    char i=20,k;
+    char m=5,n;
 
-    cout<<"max"<<(int)x<<" and "<<"min"<<(int)y<<endl;
+//FOR AND BIT
+    z=x&y;
+    cout<<z<<endl;
 
+//FOR OR BIT
+    c=a|b;
+    cout<<c<<endl;
+
+//FOR X-OR BIT
+    f=d^e;
+    cout<<f<<endl;
+
+//FOR LEFTSHIFTING CHARACTERS
+    h=j<<1;
+    cout<<(int)h<<endl;
+
+//FOR RIGHTSHIFTING A CHARACTER
+    k=i>>1;
+    cout<<k<<endl;
+
+//FOR NOT
+    n=~m;
+    cout<<n<<endl;
     return 0;
 }
