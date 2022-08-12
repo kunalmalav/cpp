a7e6ea4701ff0d8113858d24bcb2a0d846c02785
Kunal Malav <kunalmalavno.1@gmail.com>
2022-08-12 11:06:00 +0530

    marksheet

--- main.cpp
+++ main.cpp
@@ -5,13 +5,21 @@ typedef int marks;
 
 int main(){
 
-    marks ;
+    marks p1,s1,s2,s3,s4,s5;
 
-    m1=50;
-    m2=90;
+    s1=73;
+    s2=63;
+    s3=60;
+    s4=53;
+    s5=35;
+    p1=56.80;
 
-    cout<<<<endl;
-    cout<<<<endl;
+    cout<<"HINDI :"<<s1<<endl;
+    cout<<"ENGLISH :"<<s2<<endl;
+    cout<<"PHYSICS :"<<s3<<endl;
+    cout<<"CHEMISTRY :"<<s4<<endl;
+    cout<<"MATHEMATICS :"<<s5<<endl;
+    cout<<"PERCENTAGE :"<<p1<<endl;
 
     return 0;
 
