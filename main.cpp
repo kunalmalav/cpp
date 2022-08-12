fb1da7d4e4dc3639bd5e866c517b9f152b9edb8e
Kunal Malav <kunalmalavno.1@gmail.com>
2022-08-12 10:29:35 +0530

    use enum

--- main.cpp
+++ main.cpp
@@ -1,37 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+enum day{mon,tue,wed,thu,fri,sat,sun};
 
 int main()
 {
-    int x=11,y=5,z;
-    int a=11,b=7,c;
-    int d=11,e=7,f;
-    char j=5,h;
-    char i=20,k;
-    char m=5,n;
+    day m,t,w,th,f,s,su;
+    m=mon;
+    t=tue;
+    w=wed;
+    th=thu;
+    f=fri;
+    s=sat;
+    su=sun;
 
-//FOR AND BIT
-    z=x&y;
-    cout<<z<<endl;
-
-//FOR OR BIT
-    c=a|b;
-    cout<<c<<endl;
-
-//FOR X-OR BIT
-    f=d^e;
-    cout<<f<<endl;
-
-//FOR LEFTSHIFTING CHARACTERS
-    h=j<<1;
-    cout<<(int)h<<endl;
-
-//FOR RIGHTSHIFTING A CHARACTER
-    k=i>>1;
-    cout<<k<<endl;
-
-//FOR NOT
-    n=~m;
-    cout<<n<<endl;
-    return 0;
+    cout<<m<<t<<w<<th<<f<<s<<su<<endl;
 }
