f4fd5874b3df9568a38b881c9467f330831671c2
Kunal Malav <kunalmalavno.1@gmail.com>
2022-08-14 11:46:34 +0530

    my roll no

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 using namespace std;
 
-typedef int marks;
+int main()
+{
+ int x;
+ cout<<"Enter Your Roll No.:";
+ cin>>x;
+    if(x==34)
+ {
+     cout<<"You Are Right";
+ }
+    else
+ {
+     cout<<"Its Not Your Roll No.";
+ }
 
-int main(){
-
-    marks p1,s1,s2,s3,s4,s5;
-
-    s1=73;
-    s2=63;
-    s3=60;
-    s4=53;
-    s5=35;
-    p1=56.80;
-
-    cout<<"HINDI :"<<s1<<endl;
-    cout<<"ENGLISH :"<<s2<<endl;
-    cout<<"PHYSICS :"<<s3<<endl;
-    cout<<"CHEMISTRY :"<<s4<<endl;
-    cout<<"MATHEMATICS :"<<s5<<endl;
-    cout<<"PERCENTAGE :"<<p1<<endl;
 
     return 0;
 
