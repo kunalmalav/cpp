09834b803d2c6df8e2054d510cf1a4f1dec542b1
Kunal Malav <kunalmalavno.1@gmail.com>
2022-09-10 18:20:51 +0530

    max in arrays

--- main.cpp
+++ main.cpp
@@ -4,12 +4,20 @@ using namespace std;
 
 int main()
 {
-    int A[5]={1,2,3,4,5};
-    int sum=0;
-    for(int i=0;i<5;i++)
+    int A[10]={5,2,3,6,9,1,4,7,8,2};
+
+    int max;
+
+    max=A[0];
+
+    for(int i=1;i<10;i++)
     {
-        sum=sum+A[i];
+            if(A[i]>max)
+            {
+                max=A[i];
+            }
+
     }
-    cout<<"sum is: " <<sum;
+        cout<<max;
     return 0;
 }
